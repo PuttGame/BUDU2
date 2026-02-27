ee81fa9d12a7a5e56d0d01dc623215204cf311f9
PuttGame <haputt22@gmail.com>
2026-02-27 16:53:32 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'budu2-v1';
+const CACHE_NAME = 'budu2-v4';
 const urlsToCache = [
   '/BUDU2/',
   '/BUDU2/index.html',
@@ -6,19 +6,25 @@ const urlsToCache = [
   '/BUDU2/assets/watermark.png',
   '/BUDU2/assets/loadscreen.png',
   '/BUDU2/assets/home.png',
-  '/BUDU2/assets/music1.mp3'
+  '/BUDU2/assets/music1.mp3',
+  '/BUDU2/assets/icon-192.png',
+  '/BUDU2/assets/icon-512.png'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).catch(() => caches.match('/BUDU2/index.html')))
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
   );
 });
